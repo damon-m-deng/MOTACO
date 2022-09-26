84fb5df1d9d3b2e2b1c6782b9b03fdb1785efc63
Mark <marksomething@gmail.com>
2022-09-26 12:41:52 -0500

    Implemented quick fix to clear console for readability.
    It's a hack so a TODO is to put in correct console clear method in near future.

--- src/com/game/controller/GameController.java
+++ src/com/game/controller/GameController.java
@@ -8,6 +8,11 @@ import java.util.Scanner;
 class GameController {
     public static void main(String[] args) {
         // prompt user to select a class, then create a hero based on user input
+        for(int i = 0; i < 1000; i++)
+        {
+            System.out.println("\b");
+        }
+        //TODO Replace this "hackey" way of clearing console with correct method in near future
         Scanner scanner = new Scanner(System.in);
         Heroes hero = HeroesFactory.createHeroes(scanner);
         System.out.println(hero);
