e58b39c8713d3a51a85ef7062dc7bca92c549ead
Nima Gopali <nimagopali@gmail.com>
2022-09-27 20:04:39 -0400

    Added UserFactory, refactored GameController and Game java files to implement User functionality.

--- src/com/game/controller/GameController.java
+++ src/com/game/controller/GameController.java
@@ -9,7 +9,6 @@ import java.util.Scanner;
 class GameController {
     public static void main(String[] args) {
         Game game = new Game();
-        game.playerSetUp();
-        game.firstMission();
+        game.start();
     }
 }
