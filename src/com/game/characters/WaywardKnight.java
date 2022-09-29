d65e6881b4d1b279ab1ea3c486e690cbeb5c9cd4
Mark <marksomething@gmail.com>
2022-09-29 09:49:53 -0500

    Wayward Knight classical shred guitar and shield smash attack multiplier

--- src/com/game/characters/WaywardKnight.java
+++ src/com/game/characters/WaywardKnight.java
@@ -1,5 +1,7 @@
 package com.game.characters;
 
+import java.util.Scanner;
+
 public class WaywardKnight extends Heroes {
     private int energy = 100;
     private Heroes heroes;
@@ -15,20 +17,57 @@ public class WaywardKnight extends Heroes {
     }
 
     @Override
-    public void useSpecialAbility(){
+    public void useSpecialAbility() {
+        while(true) {
+            System.out.println("What Wayward Knight skill you would like to use?");
+            System.out.println("1: Shred Classical Guitar: Mesmerize the enemy with your guitar " +
+                    "skills and increase your attack damage by 1.5x");
+            System.out.println("2. Shield Smash: Smash your enemy and multiply your attack damage" +
+                    " by 1.3x");
+            System.out.println("Wayward Knight energy = "+ getEnergy());
+            Scanner scanner = new Scanner(System.in);
+            String userInput = scanner.nextLine();
+            if ("1".equals(userInput)) {
+                shredClassicalGuitar();
+                break;
+            } else if ("2".equals(userInput)) {
+                shieldSmash();
+                break;
+            } else {
+                System.out.println("please select a skill, 1 or 2");
+                System.out.println();
+            }
+        }
 
     }
 
     public void shredClassicalGuitar() {
         System.out.println("Shredding classical guitar and enemies are stunned");
-    }
+        int shreddingDamage = (int) (getAttackPower() * 1.5);
+        System.out.println("The damage is increased by a multiple of 1.5 " + shreddingDamage);
+        System.out.println();
+        setAttackPower(shreddingDamage);
+        if (energy > 20) {
+            energy = energy - 20;
+        } else {
+            System.out.println("Not enough energy...");
+        }
 
-    public void standardSwordAttacks() {
-        System.out.println("Slice and dice with in the standard fashion as one does");
     }
 
     public void shieldSmash() {
         System.out.println("Smash the enemy with your shield, like a cheap version of the hulk");
+        int shieldDamage = (int) (getAttackPower() * 1.3);
+        System.out.println("The attack power is increased by a multiple of 1.3 to " + shieldDamage);
+        System.out.println();
+
+
+        setAttackPower(shieldDamage);
+        if (energy > 30) {
+            energy = energy - 30;
+        } else {
+            System.out.println("Not enough energy...");
+        }
     }
 
     public int getEnergy() {
